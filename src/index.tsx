0e32e5eb07caf7d323c01069302976e48758b231
Skorupski <dominik.skorupski@capgemini.com>
2021-01-09 20:38:14 +0100

    add react create app and typescript and styles environment

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+
+import { unregister } from './core'
+import { GlobalStyles, theme } from './styles'
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <div>Hello World</div>
+  </ThemeProvider>,
+  document.getElementById('root')
+)
+
+unregister()
